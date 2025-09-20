c599dd3d18aef15d0202ddbb1c5ae7e164405168
Haukka10 <sokoskib@gmail.com>
2025-09-20 23:34:47 +0200

    Set a code for the level change trigger

---
+++ Source/ArcherPuzzle/Private/LevelChange/LevelChangeTrigger.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LevelChange/LevelChangeTrigger.h"
+
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ALevelChangeTrigger::ALevelChangeTrigger()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALevelChangeTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALevelChangeTrigger::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ALevelChangeTrigger::ChangeLevel() const
+{
+	if (LevelName.IsNone())
+	{
+		UE_LOG(LogTemp,Error,TEXT("Name of the level is empty"));
+		return;
+	}
+	
+	UGameplayStatics::OpenLevel(GetWorld(), LevelName);
+}
+
---
+++ Source/ArcherPuzzle/Public/LevelChange/LevelChangeTrigger.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LevelChangeTrigger.generated.h"
+
+UCLASS()
+class ARCHERPUZZLE_API ALevelChangeTrigger : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALevelChangeTrigger();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="LevelProp|Name")
+	FName LevelName = NAME_None;
+
+	UFUNCTION(BlueprintCallable)
+	void ChangeLevel() const;
+
+};
